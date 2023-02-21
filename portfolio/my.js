b46a7026a5cdf187e13592d0773ae339496f8b1b
Konda Manish kumar <86883607+kondamanish@users.noreply.github.com>
2023-02-21 19:18:51 +0530

    created a portfolio site

---
+++ portfolio/my.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.cam').style.display = "none";
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.side-bar').classList.toggle('.sidebarGo');
+})
